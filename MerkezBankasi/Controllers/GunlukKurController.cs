5228e4d9b3ae1cb0328dbaa2c901d979a14b3c23
Serinkan <Serinkan@DESKTOP-5B4HUNJ>
2024-05-07 13:23:37 +0300

    repo committed

---
+++ MerkezBankasi/Controllers/GunlukKurController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+
+namespace MerkezBankasi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class GunlukKurController : ControllerBase
+	{
+		[HttpPost]
+		public ResponseData Run(RequestData request )
+		{
+			ResponseData result = new ResponseData();
+			try
+			{
+
+				string tcmblink = string.Format("http://www.tcmb.gov.tr/kurlar/{0}.xml",
+				(request.IsBugun) ? "today" : string.Format("{2}{1}/{0}{1}{2}"
+				, request.Gun.ToString().PadLeft(2, '0'), request.Ay.ToString().PadLeft(2, '0'), request.Yil
+
+				)
+				);
+				result.Data = new List<ResponseDataKur>();
+
+				XmlDocument doc = new XmlDocument();
+
+				doc.Load(tcmblink);
+
+				if(doc.SelectNodes("Tarih_Date").Count<1)
+				{
+					result.Hata = "Kur bilgisi bulunamadı.";
+
+					return result;
+				}
+				foreach(XmlNode node in doc.SelectNodes("Tarih_Date")[0].ChildNodes)
+				{
+					ResponseDataKur kur = new ResponseDataKur();
+
+					kur.Kodu = node.Attributes["Kod"].Value;
+					kur.Adi = node["Isim"].InnerText;
+					kur.Birimi = int.Parse(node["Unit"].InnerText);
+					kur.AlisKuru = Convert.ToDecimal("0" + node["ForexBuying"].InnerText.Replace(".", ","));
+					kur.SatisKuru = Convert.ToDecimal("0" + node["ForexSelling"].InnerText.Replace(".", ","));
+					kur.EfektifSatisKuru = Convert.ToDecimal("0" + node["BanknoteBuying"].InnerText.Replace(".", ","));
+					kur.EfektifAlisKuru = Convert.ToDecimal("0" + node["BanknoteSelling"].InnerText.Replace(".", ","));
+
+					result.Data.Add(kur);
+				}
+			}
+			catch (Exception ex)
+			{ 
+				result.Hata = ex.Message;
+			}
+
+
+
+			return result;
+		}
+	}
+}
